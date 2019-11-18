3518cfaa4117c5a85dce9aa849c94f2166c05fbd
BrunoPicolo <picolobruno@hotmail.com>
2019-11-18 10:49:57 +0100

    Création classe de test correspondant à l'exigence #1

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,24 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @Test
+    public void testLoginExists() {
+    }
+
+    @Test
+    public void testAddLogin() {
+    }
+
+    @Test
+    public void testFindAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void testFindAllLogins() {
+    }
+}
