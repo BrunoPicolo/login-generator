8092bd9acf7880f54ce3d6d96bcc7c0176c6d8d3
BrunoPicolo <picolobruno@hotmail.com>
2019-11-25 19:45:03 +0100

    fix #3 of loginGenerator

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -26,5 +26,29 @@ public class LoginGeneratorTest {
         final String login1 = loginGenerator.generateLoginForNomAndPrenom(n1,p1);
         // THEN
         Assert.assertEquals("New login", "PDUR", login1);
+
+        // GIVEN
+        final String n2 = "Ralling";
+        final String p2 = "John";
+        // WHEN
+        final String login2 = loginGenerator.generateLoginForNomAndPrenom(n2,p2);
+        // THEN
+        Assert.assertEquals("New login", "JRAL2", login2);
+
+        // GIVEN
+        final String n3 = "Rolling";
+        final String p3 = "Jean";
+        // WHEN
+        final String login3 = loginGenerator.generateLoginForNomAndPrenom(n3,p3);
+        // THEN
+        Assert.assertEquals("New login", "JROL1", login3);
+
+        // GIVEN
+        final String n4 = "Dùrant";
+        final String p4 = "Paul";
+        // WHEN
+        final String login4 = loginGenerator.generateLoginForNomAndPrenom(n4,p4);
+        // THEN
+        Assert.assertEquals("New login", "PDUR1", login4);
     }
 }
